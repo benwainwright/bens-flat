d08d82026deaddcc674cf88ee1f83421515346d1
Ben Wainwright <bwainwright28@gmail.com>
2024-10-04 14:47:01 +0100

    fix: remove debug logging

--- hass-blocks-frontend/src/data/update-automations.ts
+++ hass-blocks-frontend/src/data/update-automations.ts
@@ -3,7 +3,7 @@ import { DatabaseApi } from "./create-api";
 
 export const updateAutomations = async (
   socket: Socket,
-  database: DatabaseApi
+  database: DatabaseApi,
 ) => {
   return new Promise<void>((accept) => {
     socket.emit("request-automations");
@@ -11,7 +11,6 @@ export const updateAutomations = async (
     socket.once(
       "automations",
       async (automationsFromLego: { id: string; name: string }[]) => {
-        console.log(automationsFromLego);
         const automations = await database.blocks.getAll({
           type: "automation",
         });
@@ -21,7 +20,7 @@ export const updateAutomations = async (
             (recievedAutomation) =>
               !automations
                 .map((automation) => automation.id)
-                .includes(recievedAutomation.id)
+                .includes(recievedAutomation.id),
           )
           .map((newAutomation) => {
             return {
@@ -33,7 +32,7 @@ export const updateAutomations = async (
 
         await database.blocks.update(...missing);
         accept();
-      }
+      },
     );
   });
 };
