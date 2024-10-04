3871c56e615e66f8b3341d83975701f049300eb7
Ben Wainwright <bwainwright28@gmail.com>
2024-10-04 13:57:32 +0100

    fix: correct host name

--- hass-blocks-frontend/src/data/get-socket.ts
+++ hass-blocks-frontend/src/data/get-socket.ts
@@ -3,19 +3,19 @@ import { io, Socket } from "socket.io-client";
 
 const LEGO_NAME = "hass-blocks";
 
-const getHassLegoSlug = async () => {
+const getHassLegoHost = async () => {
   const {
     data: { addons },
   } = await makeHassRequest("/addons");
 
   const lego = addons.find((addon) => addon.slug.endsWith(LEGO_NAME));
 
-  return lego?.slug ?? "";
+  return (lego?.slug ?? "").replace(/_/g, "-");
 };
 
 export const getHassBlocksSocket = async () => {
   const HASS_BLOCKS_HOST =
-    process.env.IN_ADDON === "true" ? await getHassLegoSlug() : "hass-blocks";
+    process.env.IN_ADDON === "true" ? await getHassLegoHost() : "hass-blocks";
 
   return new Promise<Socket>((accept, reject) => {
     const url = `ws://${HASS_BLOCKS_HOST}:3001`;
